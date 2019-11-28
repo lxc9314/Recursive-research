ac7f5539adf8cd2973f4b3a7b44ec28f1a809864
xuechao.li <ext.xuechao.li@ef.com>
2019-11-28 15:29:30 +0800

    add forEachDeep.js

--- forEachDeep.js
+++ forEachDeep.js
@@ -1,3 +1,25 @@
 const dataJson = require("./data.json");
-console.log(dataJson);
-const forEachDeep = data => {};
+const isPlainObject = require("is-plain-object");
+// 根据 version key-value
+const forEachDeep = (data, path, fn) => {
+  if (Array.isArray(data)) {
+    data.forEach((value, index) => {
+      let deepPath = `${path}${index}`;
+      fn(value, deepPath);
+      forEachDeep(data[index], deepPath, fn);
+    });
+  } else if (isPlainObject(data)) {
+    Object.keys(data).forEach(key => {
+      let deepPath = `${path}${key}`;
+      fn(data[key], deepPath);
+      forEachDeep(data[key], deepPath, fn);
+    });
+  }
+};
+
+const fn = (data, path) => {
+  if (typeof data === "string") {
+    console.log(path, "---", data);
+  }
+};
+const ad = forEachDeep(dataJson, "", fn);
